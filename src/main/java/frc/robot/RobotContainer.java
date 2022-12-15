03f1594a8a6112769c0ba15be6ee41a36eb3afd5
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-12-15 15:12:12 -0500

    updates from pathplanner paths

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,39 @@
 package frc.robot;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.commands.DefaultDrive;
-import frc.robot.commands.TurnWheelsToAngle;
-import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
+import frc.robot.commands.TeleopDrive;
+import frc.robot.commands.TurnWheelsToAngle;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Drivetrain.DriveMode;
 
 /**
  * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -47,7 +67,7 @@ public class RobotContainer {
                 XboxController driverController = new XboxController(Constants.OI.DRIVER_PORT);
                 // Driver left joystick and right joystick, drive
                 drivetrain.setDefaultCommand(
-                        new DefaultDrive(
+                        new TeleopDrive(
                                 () -> driverController.getLeftX(),
                                 () -> driverController.getLeftY(),
                                 () -> driverController.getRightY(),
@@ -58,19 +78,31 @@ public class RobotContainer {
                 Joystick joystick = new Joystick(0);
 
                 drivetrain.setDefaultCommand(
-                        new DefaultDrive(
-                                () -> joystick.getY() * 2.0, // because flight stick goes -0.5 to 0.5
-                                () -> joystick.getX() * 2.0,
-                                () -> joystick.getTwist() * 2.0,
+                        new TeleopDrive(
+                                () -> -joystick.getY() * 2.0, // because flight stick goes -0.5 to 0.5
+                                () -> -joystick.getX() * 2.0,
+                                () -> -joystick.getTwist() * 2.0,
                                 () -> joystick.getRawButton(1),
                                 drivetrain));
 
-                new JoystickButton(joystick, 7).whenPressed(new InstantCommand(drivetrain::resetGyroAngle));
+                new JoystickButton(joystick, 12).whenPressed(
+                        new InstantCommand(drivetrain::resetGyroAngle).beforeStarting(new PrintCommand("resetting")));
 
                 new JoystickButton(joystick, 4)
                         .whenPressed(new InstantCommand(() -> Constants.Teleop.PERCENT_LIMIT -= 0.05));
                 new JoystickButton(joystick, 6)
                         .whenPressed(new InstantCommand(() -> Constants.Teleop.PERCENT_LIMIT += 0.05));
+
+                new JoystickButton(joystick, 6).whileHeld(new RunCommand(() -> drivetrain.setModuleStates(
+                        new SwerveModuleState[] {
+                                new SwerveModuleState(0, new Rotation2d(0)),
+                                new SwerveModuleState(0, new Rotation2d(0)),
+                                new SwerveModuleState(0, new Rotation2d(0)),
+                                new SwerveModuleState(0, new Rotation2d(0))
+                        })));
+
+                new JoystickButton(joystick, 3).whenPressed(new InstantCommand(drivetrain::turnCCW90));
+                new JoystickButton(joystick, 4).whenPressed(new InstantCommand(drivetrain::turnCW90));
 
                 new POVButton(joystick, 0).whenPressed(new TurnWheelsToAngle(0.0 * Math.PI / 4.0, drivetrain));
                 new POVButton(joystick, 45).whenPressed(new TurnWheelsToAngle(1.0 * Math.PI / 4.0, drivetrain));
@@ -86,6 +118,70 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return chooser.getSelected();
+        PathPlannerTrajectory path = PathPlanner.loadPath("TestPath", 1, 3);
+        // return new SequentialCommandGroup(
+        // new InstantCommand(() -> {
+        // // Reset odometry for the first path you run during auto
+        // drivetrain.resetOdometry(path.getInitialPose());
+        // }
+        // ));
+
+        ProfiledPIDController thetaController = new ProfiledPIDController(
+                Constants.Drivetrain.PID.Drive.kP, 0, 0, new TrapezoidProfile.Constraints(
+                        Constants.Drivetrain.Geometry.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+                        Constants.Drivetrain.Geometry.MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED));
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        return new PPSwerveControllerCommand(
+                path,
+                drivetrain::getPose, // Pose supplier
+                Constants.Drivetrain.Geometry.KINEMATICS, // SwerveDriveKinematics
+                new PIDController(Constants.Drivetrain.PID.Drive.kP, 0, 0),
+                new PIDController(Constants.Drivetrain.PID.Drive.kP, 0, 0),
+                thetaController, // Rotation controller. Tune these values for your robot. Leaving them 0 will
+                                 // only use feedforwards.
+                drivetrain::setModuleStates, // Module states consumer
+                drivetrain // Requires this drive subsystem
+        ).beforeStarting(() -> drivetrain.resetOdometry(path.getInitialPose())).andThen(drivetrain::stop);
     }
+
+    // TrajectoryConfig config = new TrajectoryConfig(
+    // Constants.Auton.MAX_VELOCITY,
+    // Constants.Auton.MAX_ACCELERATION)
+    // // Add kinematics to ensure max speed is actually obeyed
+    // .setKinematics(Constants.Drivetrain.Geometry.KINEMATICS);
+
+    // // An example trajectory to follow. All units in meters.
+    // Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+    // // Start at the origin facing the +X direction
+    // new Pose2d(0, 0, new Rotation2d(0)),
+    // // Pass through these two interior waypoints, making an 's' curve path
+    // List.of(new Translation2d(0.5, -0.5)),
+    // // End 3 meters straight ahead of where we started, facing forward
+    // new Pose2d(1, 0, new Rotation2d(0)),
+    // config);
+
+    // var thetaController = new ProfiledPIDController(
+    // Constants.Drivetrain.PID.Drive.kP, 0, 0, new TrapezoidProfile.Constraints(
+    // Constants.Drivetrain.Geometry.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+    // Constants.Drivetrain.Geometry.MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED));
+    // thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    // SwerveControllerCommand swerveControllerCommand = new
+    // SwerveControllerCommand(
+    // exampleTrajectory,
+    // drivetrain::getPose, // Functional interface to feed supplier
+    // Constants.Drivetrain.Geometry.KINEMATICS,
+    // // Position controllers
+    // new PIDController(Constants.Drivetrain.PID.Drive.kP, 0, 0),
+    // new PIDController(Constants.Drivetrain.PID.Drive.kP, 0, 0),
+    // thetaController,
+    // drivetrain::setModuleStates,
+    // drivetrain);
+
+    // // Reset odometry to the starting pose of the trajectory.
+    // drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // // Run path following command, then stop at the end.
+    // return swerveControllerCommand.andThen(() -> drivetrain.drive(0, 0, 0,
+    // DriveMode.ROBOT_RELATIVE));
 }
